6f42704e91120820ef75d320e20840c74c6d604c
IlyaMotovilov <61605551+IlyaMotovilov@users.noreply.github.com>
2020-07-08 17:06:20 +0300

    Add files via upload

---
+++ lw22/lw22.1/221.pas
@@ -0,0 +1,67 @@
+
+PROGRAM InsertionSort(INPUT, OUTPUT);
+{Сортирует символы из INPUT}
+CONST
+  Max = 16;
+  ListEnd = 0;
+TYPE
+  RecArray = ARRAY [1 .. Max] OF 
+               RECORD
+                 Key: CHAR;
+                 Next: 0 .. Max;
+               END;
+VAR
+  Arr: RecArray;
+  First, Index: 0 .. Max;
+  Prev, Curr: 0 .. Max;  
+  Extra: CHAR;
+  Found: BOOLEAN;
+BEGIN {InsertionSort}
+  First := 0;
+  Index := 0;
+  WHILE NOT EOLN      
+  DO
+    BEGIN
+      {Помещать запись в список, если позволяет пространство, 
+      иначе игнорировать и сообщать об ошибке}
+      Index := Index + 1;
+      IF Index > Max
+      THEN
+        BEGIN
+          READ(Extra);
+          WRITELN('Сообщение содержит: ', Extra, '. Игнорируем.');
+        END
+      ELSE
+        BEGIN
+          READ(Arr[Index].Key);
+          Prev := 0;
+          Curr := First;
+          Found := FALSE;
+          WHILE (Curr <> 0) AND (NOT Found)
+          DO
+            IF Arr[Index]. Key > Arr[Curr].Key
+            THEN
+              BEGIN
+                Prev := Curr;
+                Curr := Arr[Curr].Next
+              END
+            ELSE
+              Found := TRUE;         {Найти значения Prev и Curr, если существуют такие что
+          Arr[Prev].Key  <= Arr[Index].Key <= Arr[Curr].Key}
+          Arr[Index].Next := Curr;
+          IF Prev = 0  {Первый элемент в списке}
+          THEN
+            First := Index
+          ELSE
+            Arr[Prev].Next := Index;{Включение Arr[Index] в связанный список}
+        END
+    END; 
+    Index := First;
+    WHILE Index <> ListEnd
+    DO
+      BEGIN
+        WRITE(Arr[Index].Key);  
+        Index := Arr[Index].Next
+      END;
+    WRITELN;{Печать списка начиная с Arr[First]}
+END.  {InsertionSort}
