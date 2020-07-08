82c8454246ba434b35969382e80e80bbcb2ab9fb
IlyaMotovilov <61605551+IlyaMotovilov@users.noreply.github.com>
2020-07-08 18:14:16 +0300

    Add files via upload

---
+++ lw23/lw23.1/231.pas
@@ -0,0 +1,45 @@
+PROGRAM InsertSort2 (INPUT, OUTPUT);
+TYPE 
+  NodePtr = ^Node;
+  Node = RECORD
+           Next: NodePtr;
+           Key: CHAR
+         END;
+VAR
+  FirstPtr, NewPtr, Curr, Prev: NodePtr;
+  Found: BOOLEAN;
+BEGIN {InsertSort2}
+  FirstPtr := NIL;
+  WHILE NOT EOLN
+  DO
+    BEGIN
+      NEW(NewPtr);
+      READ(NewPtr^.Key);
+      Prev := NIL;{2.1. Поместить NewPtr в надлежащее место}
+      Curr := FirstPtr;
+      Found := FALSE;{2.1.1 Найдем значение Prev и Curr, такие что Prev^.Key <= NewPtr^.Key <= Curr^.Key}
+      WHILE (Curr <> NIL) AND NOT Found
+      DO
+        IF NewPtr^.Key > Curr^.Key
+        THEN
+          BEGIN
+            Prev := Curr;
+            Curr := Curr^.Next;
+          END
+        ELSE
+          Found := TRUE; {2.1.1 Найдем значение Prev и Curr, такие что Prev^.Key <= NewPtr^.Key <= Curr^.Key}
+      NewPtr^.Next := Curr;
+      IF Prev = NIL 
+      THEN
+        FirstPtr := NewPtr
+      ELSE
+        Prev^.Next := NewPtr;{2.1. Поместить NewPtr в надлежащее место}
+    END;
+  NewPtr := FirstPtr;{2.2. Печать значений начиная с FirstPtr^.Key}
+  WHILE NewPtr <> NIL
+  DO
+    BEGIN
+      WRITE(NewPtr^.Key);
+      NewPtr := NewPtr^.Next
+    END{2.2. Печать значений начиная с FirstPtr^.Key}
+END.  {InsertSort2}
